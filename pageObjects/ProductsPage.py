e68a45ac01974f35e0654567f5933f904d149596
caioaza <caioaza@gmail.com>
2025-03-31 20:56:36 +0100

    Initial commit for OpenCart QA Simulation project

--- pageObjects/ProductsPage.py
+++ pageObjects/ProductsPage.py
@@ -1,8 +1,5 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-
 from OpenCartDemoSimulation.utilities.BaseClass import BaseClass
-from selenium.webdriver.support import expected_conditions as EC
 
 class ProductsPage(BaseClass):
 
