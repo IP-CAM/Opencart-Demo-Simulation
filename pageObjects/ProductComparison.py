5b983aefc4efabe73a00c9de95c7de25b2bd9d85
caioaza <caioaza@gmail.com>
2025-05-14 14:23:13 +0100

    Commit Final

--- pageObjects/ProductComparison.py
+++ pageObjects/ProductComparison.py
@@ -11,7 +11,9 @@ class ProductComparison(BaseClass):
     products = (By.CSS_SELECTOR, "#product-compare table tr:first-child td:nth-child(n+2)") #selects all <td> elements from the second one onward (so it excludes the first cell which contains "Product").
     products_name = (By.CSS_SELECTOR, "a")
     add_to_cart_button = (By.CSS_SELECTOR,"form #button-confirm")
-    remove_button = (By.CSS_SELECTOR, "form a")
+    remove_button = (By.XPATH, ".//a[contains(text(),'Remove')]")
+    last_row = (By.CSS_SELECTOR, "#product-compare table tr")
+    columns = (By.CSS_SELECTOR, "td")
 
 
     def get_products(self):
@@ -23,8 +25,30 @@ class ProductComparison(BaseClass):
     def click_add_to_cart_button(self,product):
         return product.find_element(*ProductComparison.add_to_cart_button).click()
 
-    def click_remove_button(self,product):
-        return product.find_element(*ProductComparison.remove_button).click()
+    def click_remove_button_by_index(self, index):
+        # Get the last row with the Remove buttons
+        last_row = self.driver.find_elements(*ProductComparison.last_row)[-1]
+        columns = last_row.find_elements(*ProductComparison.columns)[1:]  # Skip the first column
+        if index < len(columns):
+            remove_btn = columns[index].find_element(*ProductComparison.remove_button)
+            self.safe_click(remove_btn)
+        else:
+            raise IndexError("Index out of range when trying to click Remove button")
+
+    def add_products_comparison_list(self):
+        from OpenCartDemoSimulation.pageObjects.ProductsPage import ProductsPage
+        products_page = ProductsPage(self.driver)
+        products_page.click_category_phone()
+        products = products_page.get_products()
+        expected_products = []
+        for product in products:
+            product_title = products_page.get_product_name(product).text
+            expected_products.append(product_title)
+            products_page.click_product_add_to_comparison(product)
+        # print(expected_products)
+        products_page.click_product_compare_button()
+        return expected_products
+
 
 
 
--- tests/test_product_comparison.py
+++ tests/test_product_comparison.py
@@ -1,3 +1,7 @@
+import time
+
+import pytest
+
 from OpenCartDemoSimulation.pageObjects.ProductsPage import ProductsPage
 from OpenCartDemoSimulation.pageObjects.ProductComparison import ProductComparison
 from OpenCartDemoSimulation.utilities.BaseClass import BaseClass
@@ -5,20 +9,14 @@ from OpenCartDemoSimulation.utilities.BaseClass import BaseClass
 
 class TestProductComparison(BaseClass):
 
+
+
+
     def test_1_verify_items(self):
         #TC1 - Verify if the items in comparison list are the correct ones
         logger = self.getLogger()
-        products_page = ProductsPage(self.driver)
         product_comparison_page = ProductComparison(self.driver)
-        products_page.click_category_phone()
-        products = products_page.get_products()
-        expected_products = []
-        for product in products:
-            product_title = products_page.get_product_name(product).text
-            expected_products.append(product_title)
-            products_page.click_product_add_to_comparison(product)
-        #print(expected_products)
-        products_page.click_product_compare_button()
+        expected_products = product_comparison_page.add_products_comparison_list()
         products_comparison = []
         products_comparison_list = product_comparison_page.get_products()
         for product_comparison in products_comparison_list:
@@ -28,3 +26,31 @@ class TestProductComparison(BaseClass):
         #print(products_comparison)
         assert set(expected_products) == set(products_comparison), f"AssertionError: Expected {expected_products}, but got {products_comparison}"
         logger.info("Assertion Passed: the products were correctly added to the comparison list")
+
+
+    def test_2_delete_items(self):
+        #TC2 - Verify is the items are being correctly deleted from comparions products list
+        logger = self.getLogger()
+        product_comparison_page = ProductComparison(self.driver)
+        product_comparison_page.add_products_comparison_list()
+        product_comparison_list = product_comparison_page.get_products()
+        for index, product_comparison in enumerate(product_comparison_list):
+            product_comparison_title = product_comparison_page.get_product_name(product_comparison)
+            if product_comparison_title == "iPhone":
+                product_comparison_page.click_remove_button_by_index(index)
+                break
+        # Verify result
+        products_comparison = []
+        product_comparison_list = product_comparison_page.get_products()
+        for product_comparison in product_comparison_list:
+            product_comparison_title = product_comparison_page.get_product_name(product_comparison)
+            if product_comparison_title != "Remove":
+                products_comparison.append(product_comparison_title)
+        expected_products_comparison_list = ['HTC Touch HD', 'Palm Treo Pro']
+        assert set(products_comparison) == set(expected_products_comparison_list), f"AssertionError: Expected {expected_products_comparison_list}, but got {products_comparison}"
+        logger.info("Assertion Passed: the product was correctly removed from the comparison list")
+
+
+
+
+
